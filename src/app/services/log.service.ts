fd8c04e7998578a51bda79aaf86bc2bef9ef918a
salah <salah.yousef.muhammad@gmail.com>
2019-01-17 21:56:45 +0200

    deploy

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -11,14 +11,22 @@ export class LogService {
   selectedLog = this.logSource.asObservable();
   
   logs: Log[] = [
-    {id:'1',text:'salah',date: new Date('12/19/2012'),amISelected:false},
-    {id:'2',text:'ali',date: new Date('12/18/2013'),amISelected:false},
-    {id:'3',text:'ahmed',date: new Date('12/15/2014'),amISelected:false}
+    {id:this.uuidv4(),text:'salah',date: new Date('12/19/2012'),amISelected:false},
+    {id:this.uuidv4(),text:'ali',date: new Date('12/18/2013'),amISelected:false},
+    {id:this.uuidv4(),text:'ahmed',date: new Date('12/15/2014'),amISelected:false}
   ];
   constructor() { }
 
   getLogs() : Observable<Log[]>{
-    return of(this.logs);
+    if (localStorage.getItem('logs') === null) {
+      this.logs = this.logs;
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+    
+    return of(this.logs.sort((a, b) => {
+      return (new Date(b.date).getTime()) - (new Date(a.date).getTime());
+    }));
   }
 
   setFormLog(log: Log) {
@@ -27,6 +35,7 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log);
+    localStorage.setItem('logs', JSON.stringify(this.logs))
   }
 
   updateLog(log: Log) {
@@ -44,5 +53,11 @@ export class LogService {
         this.logs.splice(i, 1);
       }
     });
+  }
+
+  uuidv4() {
+    return ([1e7] as any +-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
   }
 }
